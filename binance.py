2984e7455f38046233cdc9ab9761f836abce1efa
Nadav <nadavf1992@gmail.com>
2023-09-07 15:50:20 +0300

    Added Binance.py pull price data functions

---
+++ binance.py
@@ -0,0 +1,28 @@
+import requests
+from helpers import smath,files
+
+BINANCE_BASE_ADRESS = 'https://api.binance.com'
+KLINE_ADRESS = '/api/v3/uiKlines'
+# number of digits Binance API expects for UTC figures
+BINANCE_UTC_INT_DIGITS = 13
+
+
+# returns data for price between two dates, for example 1-1-2022 to 1-10-2022
+def get_price_data_by_segment(coin,start_time:int,end_time:int):
+    start_time = smath.straighten_digits(start_time,BINANCE_UTC_INT_DIGITS)
+    end_time = smath.straighten_digits(end_time, BINANCE_UTC_INT_DIGITS)
+    symbol = coin.upper() + 'USDT'
+    params = {'symbol': symbol, 'interval': '1d', 'startTime': start_time, 'endTime': end_time}
+    url = BINANCE_BASE_ADRESS + KLINE_ADRESS
+    r = requests.get(url, params=params).json()
+    return r
+
+
+# returns data for last X days of price
+def get_price_data_by_limit(coin,limit):
+    symbol = coin.upper() + 'USDT'
+    params = {'symbol': symbol, 'interval': '1d', 'limit': limit}
+    url = BINANCE_BASE_ADRESS + KLINE_ADRESS
+    r = requests.get(url, params=params).json()
+    return r
+
